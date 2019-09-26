8f7bb68121a2f851bb4218f50a453fc5f28fa9ae
David Torosyan <davidtorosyan.git@gmail.com>
2019-09-25 19:13:53 -0700

    Fix webserver paths so they work locally

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,6 +7,7 @@ module.exports = merge(common, {
   devtool: 'inline-source-map',
   devServer: {
     contentBase: path.resolve(__dirname, 'web'),
+    publicPath: '/dist/',
     port: 9080
   }
 })
