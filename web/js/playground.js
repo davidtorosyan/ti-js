7011c382bb4c24ffdc7f08d594e67b1b36d71ef0
David Torosyan <davidtorosyan.git@gmail.com>
2020-12-27 15:39:50 -0800

    Update style, add tests to github pages

--- web/js/playground.js
+++ web/js/playground.js
@@ -17,20 +17,99 @@ const DEFAULT_SETTINGS = {
 // ----- On ready -----
 
 $(() => {
+  initFonts()
+  initPage()
   initInput()
   configureTranspiler()
 })
 
+function initFonts() {
+  $("head").append($(`\
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Cinzel:wght@700&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;500;700&display=swap" rel="stylesheet">
+  `))
+}
+
+function initPage() {
+  $('#content').append($(`\
+  <h1><a href="../">ti-js</a> playground</h1>
+  <div class="playground">
+      <div>
+          <label for="options">Options</label>
+          <div id="options">
+              <label for="debug">Debug</label>
+              <input id="debug" type="checkbox"></input>
+
+              <label for="persist">Persist</label>
+              <input id="persist" type="checkbox"></input>
+
+              <label for="frequency">Frequency</label>
+              <input id="frequency" type="number"
+                  min="1" max="1000" step="10"></input>
+          </div>
+      </div>
+      <div>
+          <label for="control">Control</label>
+          <div id="control">
+              <svg class="status">
+                  <circle id="daemonStatus" />
+              </svg>
+              <button id="run">Run</button>
+          </div>
+      </div>
+      <div>
+          <table>
+              <thead>
+                  <tr>
+                      <th>Source</th>
+                      <th>Output</th>
+                      <th>Input</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>
+                          <textarea id="source" autofocus rows="15" cols="20"></textarea>
+                      </td>
+                      <td>
+                          <textarea id="output" readonly rows="15" cols="20"></textarea>
+                      </td>
+                      <td>
+                          <textarea id="input" rows="15" cols="20"></textarea>
+                      </td>
+                  </tr>
+              </tbody>
+          </table>
+          </div>
+      <div>
+          <label for="transpiled">AST</label>
+          <textarea id="transpiled" readonly rows="15" cols="100"></textarea>
+      </div>
+
+  </div>
+`))
+}
+
 function initInput () {
+  const $source = $('#source');
+
   bindCheckbox($('#debug'), DEBUG_SETTING)
-  bindCheckbox($('#persist'), PERSIST_SETTING)
+  bindCheckbox($('#persist'), PERSIST_SETTING, value => {
+    if (value === true) {
+      saveToStorage(SOURCE_SETTING, $source.val())
+    }
+  })
   bindNumber($('#frequency'), FREQUENCY_SETTING)
 
   bindTextarea(
-    $('#source'),
+    $source,
     SOURCE_SETTING,
     () => getFromStorage(PERSIST_SETTING),
-    callback => $.get('/api/loop.8xp.txt', callback))
+    callback => callback(`\
+For(X,1,3,1)
+Disp X
+End`))
 }
 
 function configureTranspiler () {
@@ -103,9 +182,10 @@ function bindTextarea ($textArea, name, condition = () => true, load = undefined
   }
 }
 
-function bindCheckbox ($checkBox, name) {
+function bindCheckbox ($checkBox, name, callback) {
   $checkBox.prop('checked', getFromStorage(name))
-  $checkBox.on('change', () => saveToStorage(name, $checkBox.is(':checked')))
+  $checkBox.on('change', () => 
+    saveToStorage(name, $checkBox.is(':checked'), callback))
 }
 
 function bindNumber ($number, name) {
@@ -118,6 +198,9 @@ function getFromStorage (name) {
   return value === null ? DEFAULT_SETTINGS[name] : value
 }
 
-function saveToStorage (name, value) {
+function saveToStorage (name, value, callback) {
   localStorage.setItem(name, JSON.stringify(value))
+  if (callback !== undefined) {
+    callback(value);
+  }
 }
