55f300bc06d633aadb1386435ea5b612ecec5dc1
David Torosyan <davidtorosyan.git@gmail.com>
2022-07-29 21:23:14 -0700

    Upgrade packages with `npx npm-check-updates -u`

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,14 +1,12 @@
 const path = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require(path.resolve(__dirname, 'webpack.config.js'))
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: path.resolve(__dirname, 'web'),
-    publicPath: '/dist/',
-    port: 9080,
-    watchContentBase: true
+    static: path.resolve(__dirname, 'web'),
+    port: 9080
   }
 })
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const path = require('path')
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const common = require(path.resolve(__dirname, 'webpack.config.js'))
 
 module.exports = merge(common, {
