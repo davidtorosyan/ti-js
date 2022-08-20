299b740008983593f52baaced3b7d069534f391e
David Torosyan <davidtorosyan.git@gmail.com>
2022-08-20 15:33:52 -0700

    Convert common to typescript

---
+++ src/common/core.ts
@@ -0,0 +1,47 @@
+// core
+// ====
+
+import * as types from './types'
+
+function error(type: string, code: any, hideSource = false) {
+  return {
+    type,
+    code,
+    hideSource,
+  }
+}
+
+function tiError(code: string, hideSource = false) {
+  return error(types.ti, code, hideSource)
+}
+
+export const DataTypeError = tiError('DATA TYPE')
+export const SyntaxError = tiError('SYNTAX')
+export const UndefinedError = tiError('UNDEFINED')
+export const LabelError = tiError('LABEL')
+export const ArgumentError = tiError('ARGUMENT')
+export const DimMismatchError = tiError('DIM MISMATCH')
+export const DivideByZeroError = tiError('DIVIDE BY 0')
+export const InvalidDimError = tiError('INVALID DIM')
+export const DomainError = tiError('DOMAIN')
+
+export function libError(code: string, hideSource = false) {
+  return error(types.lib, code, hideSource)
+}
+
+export const UnimplementedError = libError('unimplemented')
+
+export function newMem() {
+  return {
+    vars: {},
+    ans: newFloat(),
+  }
+}
+
+export function newFloat(value = 0) {
+  return { type: types.NUMBER, float: value }
+}
+
+export const ONE = newFloat(1)
+
+export const MINUSONE = newFloat(-1)
