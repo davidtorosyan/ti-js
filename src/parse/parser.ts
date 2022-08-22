2f0bb219a0c40c5b138adf348ce1704952fad2a4
David Torosyan <davidtorosyan.git@gmail.com>
2022-08-21 22:10:01 -0700

    Start to convert parser to typescript

---
+++ src/parse/parser.ts
@@ -0,0 +1,62 @@
+// parser
+// ======
+
+import * as types from '../common/types'
+import { parser } from 'peggy'
+import peggyParser from './tibasic.peggy'
+
+type ParseOptions = {
+  sourceMap?: string
+}
+
+export function parse(source: string, options: ParseOptions = {}) {
+  if (source === undefined) {
+    throw new Error('Undefined source!')
+  }
+
+  const sourceMap = options.sourceMap || 'inline'
+
+  // TODO:
+  // * Allow multiple statements per line with ':'
+  const sourceLines = source.split(/\r?\n/)
+  const parsedLines = sourceLines.map(s => {
+    let parsedLine
+    try {
+      parsedLine = peggyParser.parse(s)
+    } catch (error: any) {
+      if (error instanceof parser.SyntaxError) {
+        parsedLine = { type: 'SyntaxError' }
+      } else {
+        throw error
+      }
+    }
+    if (sourceMap === 'inline') {
+      parsedLine.source = s
+    }
+    return parsedLine
+  })
+
+  return parsedLines
+}
+
+export function parseExpression(source: String) {
+  if (source === undefined) {
+    throw new Error('Undefined source!')
+  }
+
+  const sourceLines = source.split(/\r?\n/)
+  if (sourceLines.length > 1) {
+    throw new Error('Too many lines for an expression')
+  }
+  const sourceLine = sourceLines[0]
+  let parsedLine
+  try {
+    parsedLine = peggyParser.parse(sourceLine)
+  } catch (error) {
+    if (error instanceof parser.SyntaxError) {
+      throw error
+    }
+  }
+
+  return parsedLine !== undefined && parsedLine.type === types.ValueStatement ? parsedLine.value : { type: 'SyntaxError' }
+}
