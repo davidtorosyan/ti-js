9cb42e658b7e9c3d3abf2920e4914e344da083ab
David Torosyan <davidtorosyan.git@gmail.com>
2023-02-07 21:07:24 -0800

    Setup input and output

--- encoding/src/index.ts
+++ encoding/src/index.ts
@@ -1,24 +1,47 @@
 import * as fs from 'fs'
 import * as path from 'path'
 import { parse } from 'csv-parse/sync'
+import { stringify } from 'csv-stringify/sync'
 
-interface TiToken {
+interface TiTokenInput {
   hex: string
   token: string
 }
 
+interface TiTokenOutput {
+    hex: string
+    token: string
+  }
+
 function main (): void {
   const input = readInput()
-  console.log(input)
+  const output = transform(input)
+  writeOutput(output)
 }
 
-function readInput (): TiToken[] {
-  const csvFilePath = path.resolve(__dirname, '../data/input.csv')
-  const headers = ['hex', 'token']
-  const fileContent = fs.readFileSync(csvFilePath, { encoding: 'utf-8' })
-  const records: TiToken[] = parse(fileContent, {
+function transform (input: TiTokenInput[]): TiTokenOutput[] {
+  return input
+}
+
+function writeOutput (output: TiTokenOutput[]): void {
+  const outputFilePath = path.resolve(__dirname, '../dist/output.csv')
+  const outDir = path.dirname(outputFilePath)
+  const result = stringify(output, {
     delimiter: ',',
-    columns: headers,
+    columns: ['hex', 'token'],
+  })
+  if (!fs.existsSync(outDir)) {
+    fs.mkdirSync(outDir)
+  }
+  fs.writeFileSync(outputFilePath, result, { encoding: 'utf-8' })
+}
+
+function readInput (): TiTokenInput[] {
+  const inputFilePath = path.resolve(__dirname, '../data/input.csv')
+  const fileContent = fs.readFileSync(inputFilePath, { encoding: 'utf-8' })
+  const records: TiTokenInput[] = parse(fileContent, {
+    delimiter: ',',
+    columns: ['hex', 'token'],
     fromLine: 2,
   })
   return records
