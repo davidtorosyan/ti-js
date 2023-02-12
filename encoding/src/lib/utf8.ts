bf7c3e5ce0e39391cb74b7344ac08074fea2edb8
David Torosyan <davidtorosyan.git@gmail.com>
2023-02-11 22:37:36 -0800

    Finish utf8 (probably)

--- encoding/src/lib/utf8.ts
+++ encoding/src/lib/utf8.ts
@@ -19,29 +19,53 @@ const utf8HexLookup = new Map([
   ['0x0080', '﹢'],
   ['0x0081', '·'],
   ['0x00B0', '˗'],
+  ['0x00BC', '√('],
+  ['0x00BD', '∛('],
   ['0x00C1', '⏨^('],
   ['0x00EB', '∟'],
   ['0x00F1', '×√'],
-  ['0x00BC', '√('],
-  ['0x00BD', '∛('],
+  ['0xBB9C', '¨'],
+  ['0xBB9D', '¿'],
+  ['0xBB9E', '¡'],
+  ['0xBB9F', 'α'],
+  ['0xBBA0', 'β'],
+  ['0xBBA1', 'γ'],
+  ['0xBBA2', 'Δ'],
+  ['0xBBA3', 'δ'],
+  ['0xBBA4', 'ε'],
+  ['0xBBA5', 'λ'],
+  ['0xBBA6', 'μ'],
+  ['0xBBA7', 'π'],
+  ['0xBBA8', 'ρ'],
+  ['0xBBA9', 'Σ'],
+  ['0xBBAB', 'φ'],
+  ['0xBBAC', 'Ω'],
+  ['0xBBAD', 'p̂'],
+  ['0xBBAE', 'χ'],
+  ['0xBBCB', 'σ'],
+  ['0xBBCC', 'ᵀ'],
+  ['0xBBD4', '&'],
+  ['0xBBDE', 'ₓ'],
+  ['0xBBDF', '┬'],
+  ['0xBBF0', '∫'],
+  ['0xBBF1', '↑'],
+  ['0xBBF2', '↓'],
+  ['0xBBF3', '√'],
 ])
 
-const accentRange: [string, string] = [
-  '0xBB6E',
-  '0xBB99',
+const accentRanges: [string, string][] = [
+  ['0xBB6E', '0xBB99'],
+  ['0xBBCD', '0xBBCD'],
+  ['0xBB9A', '0xBB9A'],
 ]
 
-const accents = [
-  '0xBBCD',
-  '0xBB9A',
-]
-
-const subscriptRange: [string, string] = [
-  '0x5D00',
-  '0x5E45',
+const subscriptRanges: [string, string][] = [
+  ['0x5D00', '0x5E45'],
+  ['0xBBDE', '0xBBEA'],
 ]
 
 const subscriptMap = new Map([
+  ['10', '⏨'],
   ['0', '₀'],
   ['1', '₁'],
   ['2', '₂'],
@@ -73,12 +97,12 @@ export function createUtf8 (record: TiTokenInput, _strict: string): string | und
     return hexLookup
   }
 
-  const accentRelated = inRange(hex, accentRange) || accents.includes(hex)
+  const accentRelated = inRanges(hex, accentRanges)
   if (accentRelated) {
     return token
   }
 
-  const needsSubscript = inRange(hex, subscriptRange)
+  const needsSubscript = inRanges(hex, subscriptRanges)
   if (needsSubscript) {
     return replaceSubscripts(token)
   }
@@ -97,8 +121,13 @@ export function createUtf8 (record: TiTokenInput, _strict: string): string | und
   return undefined
 }
 
-function inRange (hex: string, range: [string, string]): boolean {
-  return hex >= range[0]! && hex <= range[1]!
+function inRanges (hex: string, ranges: [string, string][]): boolean {
+  for (const range of ranges) {
+    if (hex >= range[0]! && hex <= range[1]!) {
+      return true
+    }
+  }
+  return false
 }
 
 function replaceSubscripts (token: string): string {
@@ -106,5 +135,5 @@ function replaceSubscripts (token: string): string {
   for (const char of token) {
     result += subscriptMap.get(char) ?? char
   }
-  return result
+  return result.replace('small', '')
 }
