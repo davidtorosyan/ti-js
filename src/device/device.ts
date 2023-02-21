ca3c9b030f94d106f7db4be5a3ac600bb124c007
David Torosyan <davidtorosyan.git@gmail.com>
2023-02-21 13:57:04 -0800

    Create device object

--- src/device/device.ts
+++ src/device/device.ts
@@ -1,4 +1,49 @@
 // device
 // ====
 
-export { Memory } from './memory'
+import { Memory } from './memory'
+
+export { Memory }
+
+export interface State {
+  debug: boolean
+  maximumLines: number
+  frequencyMs: number
+  rows: number
+  columns: number
+  io: iolib.IoOptions
+}
+
+export interface State {
+  rows: number
+  columns: number
+  io: iolib.IoOptions
+}
+
+export interface State {
+  resume: ((callback?: () => void) => void) | undefined
+  resumeCallback: (() => void) | undefined
+  callback?: (status: string) => void
+}
+
+export interface State {
+  sourceLines: string[] | undefined
+  lines: types.Line[]
+}
+
+export interface State {
+  searchLabel: string | undefined
+  ifResult: boolean | undefined
+  incrementDecrementResult: boolean | undefined
+  linesRun: number
+  blockStack: number[]
+  falsyStackHeight: number | undefined
+  falsyBlockPreviousIf: boolean | undefined
+  i: number
+  lines: types.Line[]
+  status: string
+}
+
+export class Device {
+  private memory: Memory = new Memory()
+}
